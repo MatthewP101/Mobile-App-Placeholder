8057548b35c96cca76a51bb0896705e7ec25d38b
MatthewP101 <dirpybird101@gmail.com>
2026-03-04 16:09:09 +1100

    declared variables

--- flutter_application_week1/lib/app_container.dart
+++ flutter_application_week1/lib/app_container.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_week1/app_text.dart';
 
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+
 class AppContainer extends StatelessWidget {
   const AppContainer({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(color: Color.fromARGB(255, 0, 0, 0)),
+      decoration: const BoxDecoration
+      ( gradient: LinearGradient(colors: [Color.fromARGB(255, 0, 0, 0), Color.fromARGB(255, 255, 0, 0)],
+       begin: startAlignment, end: endAlignment)),
       child: const Center(
         child:AppText(),
       ),
